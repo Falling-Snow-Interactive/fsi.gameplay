d919b590e768806ef3bb2dfbcdf1e85b97416ad8
Kira Dinan <kira.dinan@fallingsnowinteractive.ca>
2025-09-01 09:58:20 -0300

    Update Randomizer.cs

--- Runtime/Randomizers/Randomizer.cs
+++ Runtime/Randomizers/Randomizer.cs
@@ -31,9 +31,16 @@ namespace Fsi.Gameplay.Randomizers
 			int weight = 0;
 			foreach (TEntry entry in entries) weight += entry.Weight;
 			return weight;
-		} // ReSharper disable Unity.PerformanceAnalysis
+		}
+		
+		// ReSharper disable Unity.PerformanceAnalysis
 		public TValue Randomize(List<TEntry> entries, int totalWeight)
 		{
+			if (entries.Count == 0 || totalWeight == 0)
+			{
+				Debug.LogWarning("There is nothing to randomize.");
+				return default(TValue);
+			}
 			int roll = Random.Range(0, totalWeight);
 			int weight = 0;
 			foreach (TEntry entry in entries)
@@ -48,11 +55,20 @@ namespace Fsi.Gameplay.Randomizers
 
 		public TValue Randomize()
 		{
+			if (Entries.Count == 0 || TotalWeight == 0)
+			{
+				return default(TValue);
+			}
 			return Randomize(Entries, TotalWeight);
 		}
 
 		public List<TValue> Randomize(int amount, bool repeats)
 		{
+			if (Entries.Count == 0 || TotalWeight == 0　|| amount == 0)
+			{
+				return new();
+			}
+			
 			if (repeats)
 			{
 				List<TValue> entries = new();
